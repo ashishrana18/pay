41d177b6e3ed30e621ed27a9d1a64b9d75d7bfe3
ashishrana18 <ashishrana1805@gmail.com>
2026-03-01 14:43:50 +0530

    initial commit

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,17 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import React from 'react';
-import { useColorScheme } from 'react-native';
+import { Stack } from "expo-router";
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+export default function RootLayout() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen name="index" options={{ title: "payLOG" }}></Stack.Screen>
+      <Stack.Screen
+        name="transaction"
+        options={{ title: "Transaction Details", headerBackButtonDisplayMode:"minimal" }}
+      ></Stack.Screen>
+      <Stack.Screen
+        name="new"
+        options={{ title: "New Transaction", headerBackButtonDisplayMode:"minimal" }}
+      ></Stack.Screen>
+    </Stack>
   );
 }
---
+++ src/app/transaction.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+function Transaction() {
+  return (
+    <View>
+      <Text>Transaction details</Text>
+    </View>
+  )
+}
+
+export default Transaction
